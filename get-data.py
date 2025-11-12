ec3f8f9acd7de4492d5f484976a6c70ffe6d67de
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-11-12 10:12:09 -0600

    Download and store the file using urllib3

--- get-data.py
+++ get-data.py
@@ -1,7 +1,11 @@
 """Get AIS data from the NOAA website"""
 import os
 
+import urllib3
+
+
 base_url = 'https://coast.noaa.gov/htdata/CMSP/AISDataHandler'
+storage_loc = os.path.join(os.getcwd(), 'ais_data')
 
 
 def get_ais_url(date: str) -> str:
@@ -15,3 +19,24 @@ def get_ais_url(date: str) -> str:
     year = date.split('-')[0]
     fname = f'ais-{date}.csv.zst'
     return os.path.join(base_url, year, fname)
+
+
+def get_storage_fname(date: str) -> str:
+    """Return the storage filename including path for the given date"""
+    return os.path.join(storage_loc, f'ais-{date}.csv.zst')
+
+
+def download_ais_data(date: str) -> None:
+    """Download the AIS data file for the given date
+
+    from: https://stackoverflow.com/a/77577568/1072246"""
+    ais_url = get_ais_url(date)
+    resp = urllib3.request('GET', ais_url, preload_content=False,
+                           headers={'User-Agent': 'Customer User Agent If Needed'})
+
+    with open(get_storage_fname(date), 'wb') as f:
+        for chunk in resp.stream(65536):
+            f.write(chunk)
+
+    resp.release_conn()
+    
