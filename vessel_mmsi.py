a1f398d7015527a906d2d012b6c6a6119d644a2b
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-11-22 16:16:14 -0600

    name_from_mmsi is a function that takes a single mmsi or a list

--- vessel_mmsi.py
+++ vessel_mmsi.py
@@ -4,4 +4,15 @@ Vessel names are all caps and match those in the AIS data"""
 mmsi_from_name = {'RB WEEKS': 368349000,
                   'MAGDALEN': 369305000}
 
-name_from_mmsi = dict([(v, k) for k, v in mmsi_from_name.items()])
+name_from_mmsi_dict = dict([(v, k) for k, v in mmsi_from_name.items()])
+
+def name_from_mmsi(mmsi: list[int] | int) -> str:
+    """Return the vessel name(s) associated with the mmsi
+
+    mmsi: A single or list of vessel numbers
+
+    returns: The vessel name or a list of vessel names"""
+    if isinstance(mmsi, list):
+        return [name_from_mmsi_dict[m] for m in mmsi]
+    else:
+        return name_from_mmsi_dict[mmsi]
