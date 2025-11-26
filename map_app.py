8107980bd0bbe2a50469ba25819a2aad2d2bdb7b
Robert Ramsdell <Robert.Ramsdell@DredgingResources.net>
2025-11-26 09:16:01 -0600

    Display map objects as separate lines using scattermap

--- map_app.py
+++ map_app.py
@@ -3,15 +3,17 @@
 from dash import Dash, dcc, html
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 import shapely.geometry
 
 from channel_def import channel_sections as geo_df
 
-lats = []
-lons = []
-names = []
 
+fig = go.Figure()
 for feature, name in zip(geo_df.geometry, geo_df.name):
+    lats = []
+    lons = []
+    names = []
     if isinstance(feature, shapely.geometry.linestring.LineString):
         linestrings = [feature]
     elif isinstance(feature, shapely.geometry.multilinestring.MultiLineString):
@@ -23,17 +25,22 @@ for feature, name in zip(geo_df.geometry, geo_df.name):
         lats = np.append(lats, y)
         lons = np.append(lons, x)
         names = np.append(names, [name]*len(y))
-        lats = np.append(lats, None)
-        lons = np.append(lons, None)
-        names = np.append(names, None)
 
-fig = px.line_map(lat=lats, lon=lons,
-                  hover_name=names,
-                  map_style="satellite",
-                  zoom=10,
-                  center={"lat": 29.49854, "lon": -94.866943},
+        fig.add_trace(go.Scattermap(mode="markers+lines",
+                                    lat=lats,
+                                    lon=lons,
+                                    marker=None,
+                                    name=names[0]))
+
+fig.update_layout(title_text='HSC Maintenance',
+                  showlegend=True,
+                  map={'style': 'satellite',
+                       'zoom': 10,
+                       'center': {'lat': 29.49854, 'lon': -94.866943}
+                       },
                   width=1000,
-                  height=1000)
+                  height=1000,
+                  )
 
 app = Dash()
 app.layout = html.Div([dcc.Graph(figure=fig)])
